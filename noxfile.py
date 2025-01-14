e7bd30b1d7ddd6f1d2280985d801f25ed5dfb869
Benjamin Crews <aceF22@gmail.com>
2025-01-13 20:52:50 -0500

    Comment out the safety nox session.
    Run safetycli in CI as separate action on the release job. We should probably have this as a separate job... but we also don't want to release a version with insecurities.

--- noxfile.py
+++ noxfile.py
@@ -28,7 +28,7 @@ python_versions = ["3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    "safety",
+    # "safety",
     "mypy",
     "tests",
     "typeguard",
