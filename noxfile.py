42a4df85d13eb00a9efc75d74af85070f10a114b
Benjamin Crews <aceF22@gmail.com>
2025-01-14 21:08:41 -0500

    Verbosity was a bad idea

--- noxfile.py
+++ noxfile.py
@@ -164,7 +164,9 @@ def tests(session: Session) -> None:
     session.install(".")
     session.install("coverage[toml]", "pytest", "pygments")
     try:
-        session.run("coverage", "run", "--parallel", "-m -v", "pytest", *session.posargs)
+        session.run(
+            "coverage", "run", "--parallel", "-m", "pytest", *session.posargs
+        )
     finally:
         if session.interactive:
             session.notify("coverage", posargs=[])
