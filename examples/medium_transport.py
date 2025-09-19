b6273ef47400c831012888d15ddc83324db4f263
Benjamin Crews <aceF22@gmail.com>
2025-09-19 14:55:24 -0400

    Expanding the Medium Transport Example.
    Putting the loads onto frames, and building up the fuselage.

--- examples/medium_transport.py
+++ examples/medium_transport.py
@@ -16,15 +16,16 @@ https://www.lockheedmartin.com/content/dam/lockheed-martin/aero/documents/sustai
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from hyperstruct import Material
 from hyperstruct import Station
-
+from hyperstruct import composite_cg
 
 # from hyperstruct.fuselage import Cover
 # from hyperstruct.fuselage import ForcedCrippling
-# from hyperstruct.fuselage import Fuselage
-# from hyperstruct.fuselage import MajorFrame
+from hyperstruct.fuselage import Fuselage
+from hyperstruct.fuselage import MajorFrame
 
 
 # Some global variables for reference
@@ -32,7 +33,7 @@ FUSELAGE_LENGTH = 12 * (97 + 15) + 9
 # Ignore the floor break transitions, since we're not matching the exact shape
 # we'll turn this into a rounded rectangle of equivalent length and width.
 FUSELAGE_DIAMETER = 12 * 14.17
-CENTER_FUSE_Z_REF = 12 * 7
+CENTER_FUSE_Z_REF = 12 * 7 + 20
 # A dict of FS locations where Major Frames exist and shapes change
 _w, _d, _z, _r = [
     FUSELAGE_DIAMETER,
@@ -40,6 +41,7 @@ _w, _d, _z, _r = [
     CENTER_FUSE_Z_REF,
     0.45 * FUSELAGE_DIAMETER,
 ]
+
 FS_DICT = {
     93: [
         "Nose Station, and NLG Bay Start",
@@ -100,7 +102,7 @@ ylim = (0, square_side)
 # print(f"y-axis depth = {ylim[1]-ylim[0]:.2f}")
 
 for i, station in enumerate(stations):
-    station.show(display=False, ax=axs[i], xlim=xlim, ylim=ylim)
+    station.show(display=False, axes=axs[i], xlim=xlim, ylim=ylim)
 
 for ax in fig.get_axes():
     ax.label_outer()
@@ -108,8 +110,76 @@ for ax in fig.get_axes():
 fig.suptitle(
     "Station Diagrams for Generic Medium Transport", fontfamily="serif", fontsize=16
 )
-plt.show()
+# plt.show()
 
+
+# An initial estimate at weight distribution based on CG target
+target_weight = 73000
+point_weights = np.array(
+    [
+        # Weight , FS
+        [1200, 93],
+        [6500, 165],
+        [9000, 245],
+        [15000, 497],
+        [16000, 620],
+        [11000, 737],
+        [7600, 941],
+        [4200, 1071],
+        [2500, 1200],
+    ]
+)
+_w, _cg = composite_cg(point_weights)
+print(f"Target Weight = {target_weight:d} [lbs]")
+print(f" Total Weight = {_w:d} [lbs]")
+print(f"           CG = {_cg:.2f} [in]")
+
+
+#
+# Landing Gear Loads Calculation
+#       Taxi, WC=73k, 1.5g
+#
+FNZ0 = 1.5
+XNGG = 240
+XMGG = 600
+XCG = 596
+DGW = target_weight
+Rmg = FNZ0 * DGW * (XCG - XNGG) / (XMGG - XNGG)
+Rng = FNZ0 * DGW - Rmg
+
+# Sink Speed is 10 ft/s, convert to in/s
+# SSPD = 12 * 10.0
+# Shock Strut Stroke
+# STKE = 26
+# g = 386     # in/s2
+
+# Assume no Airloads for now
+PZN = 0  # Forebody lift
+XCPN = 170  # Forebody Center of pressure
+PZWB = 0  # Wing outer panel lift
+XCPW = 620  # Coord of outer wing panel
+PZBW = 0  # Body lift in presence of wing
+XCPB = 600  # Coord of of body lift
+PZH = 0  # Horz Tail Lift
+XCPH = 1200  # Coord of hTail lift
+
+# Vehicle Pitch Inertia, simplified. See pg 56 fo ADA002867
+TIYY = np.sum(point_weights[:, 0] * (XCG - point_weights[:, 1]) ** 2)
+print(f"Vehicle pitch inertia = {TIYY:.3e}")
+
+print(f"\nLoads for {FNZ0}g Taxi:")
+print(f" Rmg = {Rmg:.3e} [lbs]")
+print(f" Rng = {Rng:.3e} [lbs]")
+print(f"FNZO = {FNZ0:.2f} [g]")
+diff = FNZ0 * DGW - Rmg - Rng
+print(
+    f"Balance of Vertical Forces: {FNZ0:.1f}*{DGW:.2e} - {Rmg:.2e} - {Rng:.2e} = {diff:.1f}"
+)
+
+
+#
+# Turn the Stations and Loads into Frames
+#
 
 # A basic aluminum material
 # 2024-T3, Sheet, A-basis
@@ -127,3 +197,81 @@ al2024 = Material(
     F_en=20.0e3,
     db_r=116,
 )
+
+
+inertia_loads = FNZ0 * point_weights[:, 0].flatten()
+
+gear_loads = [
+    #   y,    z,     V,   H,   M
+    np.array(
+        [  # FS 165
+            [20.0, 17.0, Rng / 2, 0.0, 0.0],  # NLG LH Mount
+            [-20.0, 17.0, Rng / 2, 0.0, 0.0],  # NLG RH Mount
+        ]
+    ),
+    np.array(
+        [  # FS 620
+            [FUSELAGE_DIAMETER / 2, 47.0, Rmg / 2, 0.0, 0.0],  # MLG LH Mount
+            [-FUSELAGE_DIAMETER / 2, 47.0, Rmg / 2, 0.0, 0.0],  # MLG RH Mount
+        ]
+    ),
+]
+
+frames = {}
+for station in stations:
+    if station.number == 165:
+        load = gear_loads[0]
+    elif station.number == 620:
+        load = gear_loads[1]
+    else:
+        load = None
+
+    frames[station.number] = MajorFrame(
+        material=al2024,
+        fs_loc=station.number,
+        loads=load,
+        geom=station,
+        fd=6.0,
+    )
+
+nlg_frame = frames.pop(165)
+mlg_frame = frames.pop(620)
+# Don't need all the frames since only 2 of them have loads
+frames = (nlg_frame, mlg_frame)
+fig1, ax1 = nlg_frame.show()
+fig2, ax2 = mlg_frame.show()
+
+
+#
+# Compile the frames and stations into a fuselage
+#
+w_fus = np.column_stack(
+    (point_weights[:, 1], FNZ0 * point_weights[:, 0], np.zeros((len(point_weights),)))
+)
+w_fc = np.zeros(3)
+p_air = np.zeros(3)
+ext_loads = [row[:, 2].sum() for row in gear_loads]
+p_ext = np.column_stack(
+    (np.array([[165], [620]]), np.transpose(ext_loads), np.zeros((2,)))
+)
+print(w_fus)
+print("")
+print(p_ext)
+
+fuse = Fuselage(stations=stations, major_frames=frames)
+loads = fuse.net_loads(w_fus, w_fc, p_air, p_ext)
+fig, (ax1, ax2) = fuse.vmt_diagram(w_fus, w_fc, p_air, p_ext)
+
+print("   FS     , P     , M_ext   , V     ,  M_int")
+print(loads)
+
+print("\n\n")
+x, v, m = fuse.lookup_loads(x=500, loads=loads)
+print(x)
+print(v)
+print(m)
+
+_ = ax1.plot(x, v, marker="^", color="k")
+_ = ax2.plot(x, m, marker="^", color="k")
+
+plt.show()
